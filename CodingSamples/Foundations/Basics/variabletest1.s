d4c2ce640e2e6c987b792a653e7a032ba4788bb3
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2026-04-14 18:03:58 +0530

    Add files via upload

---
+++ CodingSamples/Foundations/Basics/variabletest1.s
@@ -0,0 +1,18 @@
+	.include "common.i"
+
+	.start
+
+	GetInt	asku			# rax=N
+	mov	rbx, rax		# rbx=N
+	add	rbx, 1			# rbx=N+1
+	imul	rax, rbx		# rax=N*(N+1)
+	shr	rax, 1			# rax=N*(N+1)/2
+	PutInt	tell			
+
+	.stop
+
+asku:	.string	"Upper Limit: "
+tell:	.string	"Computation Result = "
+
+	.end
+
