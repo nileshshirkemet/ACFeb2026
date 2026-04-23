c4cb4fd2c8fc800a849d297126c11114bbc11468
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2026-04-23 11:34:49 +0530

    Add files via upload

---
+++ CodingSamples/Foundations/Polymorphism/banners.h
@@ -0,0 +1,48 @@
+class Banner
+{
+public:
+    //using member variable initializer syntax allowed only for constructor
+    Banner(float w = 20, float h = 3) : width(w), height(h)
+    {
+    }
+
+    void Resize(float w, float h)
+    {
+        width = w;
+        height = h;
+    }
+
+    //only a member function declared with 'virtual' keyword can be
+    //overridden in the derived class
+    virtual double Material() const
+    {
+        return width * height;
+    }
+
+private:
+    float width;
+    float height;
+};
+
+//defining HardBanner as a derived class of Banner (base class)
+class HardBanner : public Banner
+{
+public:
+    //the constructor of derived class must call constructor of base class
+    //to initialize the base-class subobject in its own instance
+    HardBanner(float t) : Banner(15, 2)
+    {
+        thickness = t;
+    }
+
+    //method overriding - defining a member function in a derived class
+    //with a declaration that exactly matches with the declaration
+    //of a virtual member function in the base class
+    double Material() const
+    {
+        return thickness * Banner::Material();
+    }
+
+private:
+    float thickness;
+};
---
+++ CodingSamples/Foundations/Polymorphism/derivedclasstest.cpp
@@ -0,0 +1,28 @@
+#include "banners.h"
+#include <cstdio>
+
+double PurchaseBanner(const Banner& each, int copies)
+{
+    float rate = copies < 5 ? 0.90 : 0.85;
+    //when a virtual method of class is called on a reference/pointer
+    //of that class type, dynamic-binding is used for dispatching
+    //the invocation
+    return copies * rate * each.Material();
+}
+
+int main(void)
+{
+    float w, h;
+    printf("Dimensions of Banner: ");
+    scanf("%f%f", &w, &h);
+    int n;
+    printf("Number of Banners   : ");
+    scanf("%d", &n);
+
+    Banner a(w, h);
+    printf("Total payment for regular banners = %.2lf\n", PurchaseBanner(a, n));
+
+    HardBanner b(1.5);
+    b.Resize(w, h);
+    printf("Total payment for premium banners = %.2lf\n", PurchaseBanner(b, n));
+}
