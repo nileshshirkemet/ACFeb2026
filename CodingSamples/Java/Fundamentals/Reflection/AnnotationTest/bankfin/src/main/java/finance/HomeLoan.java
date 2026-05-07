6a74ed37c618af4635fcf72be38eab4cb990410e
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2026-05-07 18:08:29 +0530

    Add files via upload

---
+++ CodingSamples/Java/Fundamentals/Reflection/AnnotationTest/bankfin/src/main/java/finance/HomeLoan.java
@@ -0,0 +1,18 @@
+package finance;
+
+public class HomeLoan {
+
+    public float common(double amount, int period) {
+        return amount < 5000000 ? 8.5f : 8.0f;
+    }
+
+    public float woman(double amount, int period) {
+        return common(amount, period) - 1;
+    }
+
+    @MaxDuration(12) //value = 12
+    public float welfare(double amount, int period) {
+        return 0.6f * common(amount, period);
+    }
+}
+
