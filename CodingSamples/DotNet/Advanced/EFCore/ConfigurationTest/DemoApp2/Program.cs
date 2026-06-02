2fe0e4feac6e6f2d63d86ddfe4b1c7c3bcba0443
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2026-06-02 18:11:37 +0530

    Add files via upload

--- CodingSamples/DotNet/Advanced/EFCore/ConfigurationTest/DemoApp2/Program.cs
+++ CodingSamples/DotNet/Advanced/EFCore/ConfigurationTest/DemoApp2/Program.cs
@@ -2,9 +2,25 @@
 using Microsoft.EntityFrameworkCore;
 
 var db = new DbContextOptionsBuilder<ShopDbContext>();
-db.UseSqlServer("Data Source=iitdac.met.edu;Database=Shop;User Id=dac;Password=Dac@1234;Encrypt=false");
+if(args.Length > 1 && args[1] == "server")
+    db.UseSqlServer("Data Source=iitdac.met.edu;Database=Shop;User Id=dac;Password=Dac@1234;Encrypt=false");
+else
+    db.UseSqlite("Data Source=data/shop.db");
 using var shop = new ShopDbContext(db.Options);
-foreach(var product in shop.Products)
+int pno = int.Parse(args[0]);
+var product = shop.Products.Find(pno);
+if(product == null)
 {
-    Console.WriteLine("{0, -6}{1, 12:0.00}{2, 8}", product.Id, product.Price, product.Stock);
+    foreach(var item in shop.Products)
+    {
+        Console.WriteLine("{0, -6}{1, 12:0.00}{2, 8}", item.Id, item.Price, item.Stock);
+    }
+}
+else
+{
+    shop.Entry(product).Collection(p => p.Orders).Load(); //explicit loading of child entities
+    foreach(var entry in product.Orders)
+    {
+        Console.WriteLine("{0}\t{1}\t{2:dd-MMM-yyyy}", entry.CustomerId, entry.Quantity, entry.OrderDate);
+    }
 }
